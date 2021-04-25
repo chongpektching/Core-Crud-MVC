d02d3bf2f58472601c250b09c89fd81b81cb67eb
chongpektching <chongpektching@gmail.com>
2021-04-25 18:11:05 +0800

    Add project files.

---
+++ Core-Crud-MVC/Controllers/EmpController.cs
@@ -0,0 +1,104 @@
+﻿using Core_Crud_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Crud_MVC.Controllers
+{
+    public class EmpController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public EmpController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var displayData = _db.EmployeeTable.ToList();
+
+            return View(displayData);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(NewEmpClass nec)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(nec);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(nec);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var results = await _db.EmployeeTable.FindAsync(id);
+
+            return View(results);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(NewEmpClass nec)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Update(nec);
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(nec);
+        }
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var results = await _db.EmployeeTable.FindAsync(id);
+
+            return View(results);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var results = await _db.EmployeeTable.FindAsync(id);
+
+            return View(results);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var results = await _db.EmployeeTable.FindAsync(id);
+
+            _db.EmployeeTable.Remove(results);
+
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Core-Crud-MVC/Models/NewEmpClass.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core_Crud_MVC.Models
+{
+    public class NewEmpClass
+    {
+        [Key]
+        public int Empid { get; set; }
+        [Required(ErrorMessage = "Enter Employee Name")]
+        [Display(Name = "Employee Name")]
+        public string Empname { get; set; }
+        [Required(ErrorMessage = "Enter Email")]
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Enter Employee Age")]
+        [Display(Name = "Age")]
+        [Range(20,50)]
+        public int Age { get; set; }
+        [Required(ErrorMessage = "Enter Employee Salary")]
+        [Display(Name = "Salary")]
+        public int Salary { get; set; }
+    }
+}
